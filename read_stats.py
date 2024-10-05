4c3b4cd0705f137e00a47e800dfef2fb2d9ce692
adrianUC <nichita@me.com>
2024-10-04 21:48:39 -0400

    add m5outs for various simulations

--- read_stats.py
+++ read_stats.py
@@ -1,22 +1,13 @@
-with open('m5out/stats.txt', 'r') as f:
+with open('m5out_64_128/stats.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
-        if "system.cpu.mmu.dtb.rdMisses" in line:
+        if "board.processor.cores.core.mmu.dtb.rdMisses" in line:
             rdMisses = int(line.split()[1].strip())
-        if "system.cpu.mmu.dtb.rdAccesses" in line:
+        if "board.processor.cores.core.mmu.dtb.rdAccesses" in line:
             rdAccesses = int(line.split()[1].strip())
-        if "system.cpu.mmu.dtb.wrMisses" in line:
+        if "board.processor.cores.core.mmu.dtb.wrMisses" in line:
             wrMisses = int(line.split()[1].strip())
-        if "system.cpu.mmu.dtb.wrAccesses" in line:
+        if "board.processor.cores.core.mmu.dtb.wrAccesses" in line:
             wrAccesses = int(line.split()[1].strip())
-        if "system.mem_ctrl.requestorReadAvgLat::cpu.inst" in line:
-            instructionRdAvgLat = float(line.split()[1].strip())
-            print("Instruction Read Average Latency: ", instructionRdAvgLat)
-        if "system.mem_ctrl.requestorReadAvgLat::cpu.data" in line:
-            dataRdAvgLat = float(line.split()[1].strip())
-            print("Data Read Average Latency: ", dataRdAvgLat)
-        if "system.mem_ctrl.dram.avgMemAccLat" in line:
-            avgMemAccLat = float(line.split()[1].strip())
-            print("Average Memory Access Latency: ", avgMemAccLat) 
     print("Read Cache Hit Rate: ", 1 - rdMisses / rdAccesses)
     print("Write Cache Hit Rate: ",  1 - wrMisses / wrAccesses)
--- run_tlb.py
+++ run_tlb.py
@@ -2,7 +2,6 @@ from gem5.utils.requires import requires
 from gem5.components.boards.x86_board import X86Board
 from gem5.components.memory.single_channel import SingleChannelDDR3_1600
 from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (MESITwoLevelCacheHierarchy,)
-from gem5.coherence_protocol import CoherenceProtocol
 from gem5.isas import ISA
 from gem5.components.processors.cpu_types import CPUTypes
 from gem5.resources.resource import Resource
@@ -11,13 +10,7 @@ from gem5.simulate.exit_event import ExitEvent
 from gem5.components.processors.simple_processor import SimpleProcessor
 
 from m5.objects import *
-
-# This runs a check to ensure the gem5 binary is compiled to X86 and supports
-# the MESI Two Level coherence protocol.
-requires(
-    isa_required=ISA.X86,
-    coherence_protocol_required=CoherenceProtocol.MESI_TWO_LEVEL,
-)
+from pathlib import Path
 
 # Here we setup a MESI Two Level Cache Hierarchy.
 cache_hierarchy = MESITwoLevelCacheHierarchy(
@@ -30,21 +23,15 @@ cache_hierarchy = MESITwoLevelCacheHierarchy(
     num_l2_banks=1,
 )
 
-# Setup the system memory.
-# Note, by default DDR3_1600 defaults to a size of 8GiB. However, a current
-# limitation with the X86 board is it can only accept memory systems up to 3GB.
-# As such, we must fix the size.
 memory = SingleChannelDDR3_1600("2GiB")
 
 processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=1)
 
 onlyCore = processor.cores[0].get_simobject()
 
-onlyCore.mmu.dtb = X86TLB(size=64, entry_type='data')  # Data TLB (64 entries, 4-way set associative)
-onlyCore.mmu.itb = X86TLB(size=64, entry_type='instruction')  # Instruction TLB (64 entries, 4-way set associative)
-
-# exit early for debugging
-# exit(0)
+# Set the TLB sizes: 64 entries
+onlyCore.mmu.dtb = X86TLB(size=128, entry_type='data')  # Data TLB (64 entries, 4-way set associative)
+onlyCore.mmu.itb = X86TLB(size=128, entry_type='instruction')  # Instruction TLB (64 entries, 4-way set associative)
 
 # Here we setup the board. The X86Board allows for Full-System X86 simulations.
 board = X86Board(
@@ -54,28 +41,19 @@ board = X86Board(
     cache_hierarchy=cache_hierarchy,
 )
 
-# This is the command to run after the system has booted. The first `m5 exit`
-# will stop the simulation so we can switch the CPU cores from KVM to timing
-# and continue the simulation to run the echo command, sleep for a second,
-# then, again, call `m5 exit` to terminate the simulation. After simulation
-# has ended you may inspect `m5out/system.pc.com_1.device` to see the echo
-# output.
-command = "m5 exit;" \
-        + "echo 'Started, creating  checkpoint...';" \
-        + "m5 checkpoint;" \
-        + "echo 'This is running on Timing CPU cores.';" \
-        + "sleep 1;" \
+# Run ps -o min_flt,maj_flt 1 to get the number of minor and major page faults
+
+command = "echo 'Started, calculating page fault rate:';" \
+        + "var=$(ps -o min_flt,maj_flt 1;);" \
+        + "echo $var;" \
         + "m5 exit;"
 
-# Here we set the Full System workload.
-# The `set_workload` function for the X86Board takes a kernel, a disk image,
-# and, optionally, a the contents of the "readfile". In the case of the
-# "x86-ubuntu-18.04-img", a file to be executed as a script after booting the
-# system.
+
 board.set_kernel_disk_workload(
     kernel=Resource("x86-linux-kernel-5.4.49",),
     disk_image=Resource("x86-ubuntu-18.04-img"),
     readfile_contents=command,
+    # checkpoint=Path("output/timing_cpu_checkpoint")
 )
 
 simulator = Simulator(
