68ead31d40abd4f7597115e46bfadaf206f5561a
adrianUC <nichita@me.com>
2024-10-19 11:01:20 -0400

    Got it running

--- residency.py
+++ residency.py
@@ -2,10 +2,105 @@
 import m5
 from m5.objects import *
 
-# Add the caches scripts to our path
-m5.util.addToPath("../")
+# m5.util.addToPath("configs/")
 
-from learning_gem5.part1.caches import *
+class L1Cache(Cache):
+    """Simple L1 Cache with default values"""
+
+    assoc = 2
+    tag_latency = 2
+    data_latency = 2
+    response_latency = 2
+    mshrs = 4
+    tgts_per_mshr = 20
+
+    def __init__(self, options=None):
+        super().__init__()
+        pass
+
+    def connectBus(self, bus):
+        """Connect this cache to a memory-side bus"""
+        self.mem_side = bus.cpu_side_ports
+
+    def connectCPU(self, cpu):
+        """Connect this cache's port to a CPU-side port
+        This must be defined in a subclass"""
+        raise NotImplementedError
+
+
+class L1ICache(L1Cache):
+    """Simple L1 instruction cache with default values"""
+
+    # Set the default size
+    size = "16kB"
+
+    # SimpleOpts.add_option(
+    #     "--l1i_size", help=f"L1 instruction cache size. Default: {size}"
+    # )
+
+    def __init__(self, opts=None):
+        super().__init__(opts)
+        if not opts or not opts.l1i_size:
+            return
+        self.size = opts.l1i_size
+
+    def connectCPU(self, cpu):
+        """Connect this cache's port to a CPU icache port"""
+        self.cpu_side = cpu.icache_port
+
+
+class L1DCache(L1Cache):
+    """Simple L1 data cache with default values"""
+
+    # Set the default size
+    size = "64kB"
+
+    # SimpleOpts.add_option(
+    #     "--l1d_size", help=f"L1 data cache size. Default: {size}"
+    # )
+
+    def __init__(self, opts=None):
+        super().__init__(opts)
+        if not opts or not opts.l1d_size:
+            return
+        self.size = opts.l1d_size
+
+    def connectCPU(self, cpu):
+        """Connect this cache's port to a CPU dcache port"""
+        self.cpu_side = cpu.dcache_port
+
+
+class L2Cache(Cache):
+    """Simple L2 Cache with default values"""
+
+    # Default parameters
+    size = "256kB"
+    assoc = 8
+    tag_latency = 20
+    data_latency = 20
+    response_latency = 20
+    mshrs = 20
+    tgts_per_mshr = 12
+
+    # SimpleOpts.add_option("--l2_size", help=f"L2 cache size. Default: {size}")
+
+    def __init__(self, opts=None):
+        super().__init__()
+        if not opts or not opts.l2_size:
+            return
+        self.size = opts.l2_size
+
+    def connectCPUSideBus(self, bus):
+        self.cpu_side = bus.mem_side_ports
+
+    def connectMemSideBus(self, bus):
+        self.mem_side = bus.cpu_side_ports
+
+
+# Add the caches scripts to our path
+m5.util.addToPath("config/")
+
+# from learning_gem5.part1.caches import *
 
 import argparse
 
@@ -74,7 +169,7 @@ system.mem_ctrl.port = system.membus.mem_side_ports
 thispath = os.path.dirname(os.path.realpath(__file__))
 binary = os.path.join(
     thispath,
-    "../../",
+    # "../../",
     "tests/test-progs/hello/bin/arm/linux/hello",
 )
 
